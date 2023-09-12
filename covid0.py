2871957d3fc34b062fa62d75a0c87cc69523e9c9
lukas <lukaszkowalski2013@gmail.com>
2023-09-12 17:35:41 +0200

    data sources added

--- covid0.py
+++ covid0.py
@@ -383,6 +383,20 @@ weekly_charts = dcc.Graph(id='weekly_charts', figure=weekly_charts_fig)
 polices_fig = create_covid_policy_sparklines_for_country_subset(polices, country='Poland')
 polices_charts = dcc.Graph(id='polices_charts', figure=polices_fig)
 
+ending_markdown_text = '''
+
+Charts and maps were created based on following data sources:
+- Excess deaths were estimated based on Eurostat data on weekly deaths [Eurostat](https://ec.europa.eu/eurostat/databrowser/view/DEMO_MEXRT__custom_309801/bookmark/table?lang=en&bookmarkId=26981184-4241-4855-b18e-8647fc8c0dd2)
+- Confirmed cases and deaths - [JHU CSSE/Our World in Data](https://github.com/CSSEGISandData/COVID-19)
+- Vaccination rate [Our World in Data](https://github.com/owid/covid-19-data)
+- Government Response Index Average and data on restrictions [Oxford COVID-19 Government Response Tracker, Blavatnik School of Government, University of Oxford.](https://github.com/OxCGRT/covid-policy-dataset)
+- Google mobility change by activity time [Google Mobility Reports] (https://www.google.com/covid19/mobility/)
+
+Broaden your knowledge here:
+- [Eurostat article on excess deaths in EU between 2020 and 2023](https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Excess_mortality_-_statistics#Excess_mortality_in_the_EU_between_January_2020_and_May_2023/)
+- Thomas Hale, Noam Angrist, Rafael Goldszmidt, Beatriz Kira, Anna Petherick, Toby Phillips, Samuel Webster, Emily Cameron-Blake, Laura Hallas, Saptarshi Majumdar, and Helen Tatlow. (2021). “A global panel database of pandemic policies (Oxford COVID-19 Government Response Tracker).” Nature Human Behaviour. https://doi.org/10.1038/s41562-021-01079-8
+'''
+
 ############ layout ############
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
@@ -407,7 +421,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     excess_deaths_explanation,
     weekly_charts_intro,
     weekly_charts,
-    polices_charts
+    polices_charts,
+    html.Div([
+        dcc.Markdown(children=ending_markdown_text),
+    ])
 ])
 
 ########## callbacks ##########
